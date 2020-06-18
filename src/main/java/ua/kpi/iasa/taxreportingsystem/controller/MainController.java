c3bfb94064551dc740ab3261220e5c221d623f98
Vladyslav Avdiienko <avdienko.vladyslav@gmail.com>
2020-06-18 04:31:25 +0300

    feat: add registration validation

--- src/main/java/ua/kpi/iasa/taxreportingsystem/controller/MainController.java
+++ src/main/java/ua/kpi/iasa/taxreportingsystem/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ua.kpi.iasa.taxreportingsystem.dto.UserDTO;
 import ua.kpi.iasa.taxreportingsystem.service.UserService;
+import ua.kpi.iasa.taxreportingsystem.util.UserValidator;
 
 @Controller
 public class MainController {
@@ -25,19 +26,32 @@ public class MainController {
     }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration(Model model){
+
+        model.addAttribute("isUserExist", false);
+        model.addAttribute("isUsernameValid", true);
+        model.addAttribute("isPasswordValid", true);
+
         return "registration";
     }
 
     @PostMapping("/registration")
     public String addUser(UserDTO userDTO, Model model){
 
-        if(userService.findByUsername(userDTO.getUsername()).isPresent()){
-            model.addAttribute("message", "User exists!");
-            return "registration";
-        }
-        userService.createUser(userDTO);
+        UserValidator userValidator = new UserValidator();
 
-        return "redirect:/login";
+        if(!userService.findByUsername(userDTO.getUsername()).isPresent()
+                && userValidator.isValidUsername(userDTO.getUsername())
+                && userValidator.isValidPassword(userDTO.getPassword())) {
+
+            userService.createUser(userDTO);
+            return "redirect:/login";
+        }
+
+        model.addAttribute("isUserExist", userService.findByUsername(userDTO.getUsername()).isPresent());
+        model.addAttribute("isUsernameValid", userValidator.isValidUsername(userDTO.getUsername()));
+        model.addAttribute("isPasswordValid", userValidator.isValidPassword(userDTO.getPassword()));
+
+        return "registration";
     }
 }
---
+++ src/main/java/ua/kpi/iasa/taxreportingsystem/util/UserValidator.java
@@ -0,0 +1,20 @@
+package ua.kpi.iasa.taxreportingsystem.util;
+
+import java.util.ResourceBundle;
+
+public class UserValidator {
+
+    private ResourceBundle regexpBundle;
+
+    {
+        regexpBundle = ResourceBundle.getBundle("regex");
+    }
+
+    public boolean isValidUsername(String username) {
+        return username.matches(regexpBundle.getString("username.regexp"));
+    }
+
+    public boolean isValidPassword(String password) {
+        return password.matches(regexpBundle.getString("password.regexp"));
+    }
+}
