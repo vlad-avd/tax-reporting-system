ccf1e5fa35329df292b6231696dfe594337adb48
Vladyslav Avdiienko <avdienko.vladyslav@gmail.com>
2020-06-18 03:28:35 +0300

    set default locale to RU

--- src/main/java/ua/kpi/iasa/taxreportingsystem/config/WebSecurityConfig.java
+++ src/main/java/ua/kpi/iasa/taxreportingsystem/config/WebSecurityConfig.java
@@ -1,18 +1,13 @@
 package ua.kpi.iasa.taxreportingsystem.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import ua.kpi.iasa.taxreportingsystem.service.UserService;
 
 @Configuration
@@ -27,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/registration").permitAll()
+                .antMatchers("/", "/registration", "/login/**").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
@@ -36,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
             .and()
                 .logout()
+                .logoutSuccessUrl("/")
                 .permitAll();
     }
 
